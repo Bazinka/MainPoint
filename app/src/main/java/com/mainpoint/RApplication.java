b9d1829204c771f3e508ab2c324b34e188350e6f
Daria Efimova <d.efimova@vectordigital.ru>
2017-02-02 17:31:05 +0100

    added twitter kit and crashlytics

--- app/src/main/java/com/mainpoint/RApplication.java
+++ app/src/main/java/com/mainpoint/RApplication.java
@@ -4,6 +4,10 @@ import android.app.Application;
 
 import com.bettervectordrawable.VectorDrawableCompat;
 
+import com.crashlytics.android.Crashlytics;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 import net.danlew.android.joda.JodaTimeAndroid;
 
 import io.realm.Realm;
@@ -14,9 +18,16 @@ import io.realm.Realm;
 
 public class RApplication extends Application {
 
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "0l4bGspRK4LNZQIwMx9AaWA1W";
+    private static final String TWITTER_SECRET = "NiNrGfXFdhVFMIw6Y1U4nue30TAU321CZWUZ2VWrhokIRNRJpm";
+
+
     @Override
     public void onCreate() {
         super.onCreate();
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Crashlytics(), new Twitter(authConfig));
         Realm.init(this);
         JodaTimeAndroid.init(this);
         findAllVectorResourceIdsSlow();
