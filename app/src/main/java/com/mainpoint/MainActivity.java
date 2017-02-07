f59e6578c7ce2f2e0c4066237c47b8d964d9d169
Daria Efimova <d.efimova@vectordigital.ru>
2017-02-07 14:50:39 +0100

    added user info to menu and sign out button

--- app/src/main/java/com/mainpoint/MainActivity.java
+++ app/src/main/java/com/mainpoint/MainActivity.java
@@ -2,8 +2,10 @@ package com.mainpoint;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,17 +13,28 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.mainpoint.auth.FirebaseAuthActivity;
 import com.mainpoint.list_points.PointListFragment;
 import com.mainpoint.map.exist_points.ExistingPointsMapFragment;
 import com.mainpoint.map.search_new_point.SearchNewPointActivity;
 import com.mainpoint.models.Point;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private int selectedNavigationId;
-    private Menu menu;
+    private int mSelectedNavigationId;
+    private Menu mMenu;
+    private View mRootView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +49,27 @@ public class MainActivity extends BaseActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+        mRootView = findViewById(R.id.drawer_layout);
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        View headerView = navigationView.getHeaderView(0);
+        if (auth != null && auth.getCurrentUser() != null) {
+            FirebaseUser user = auth.getCurrentUser();
+            if (user.getPhotoUrl() != null) {
+                ImageView photoImageView = (ImageView) headerView.findViewById(R.id.user_photo_imageView);
+                Picasso.with(this).load(user.getPhotoUrl().getPath()).placeholder(R.drawable.logo).into(photoImageView);
+            }
+            if (user.getDisplayName() != null) {
+                TextView nameTextView = (TextView) headerView.findViewById(R.id.user_name_textView);
+                nameTextView.setText(user.getDisplayName());
+            }
+        }
+
         navigationView.setNavigationItemSelectedListener(this);
 
-        selectedNavigationId = R.id.nav_map;
-        navigationView.setCheckedItem(selectedNavigationId);
+        mSelectedNavigationId = R.id.nav_map;
+        navigationView.setCheckedItem(mSelectedNavigationId);
         setPointsMapFragment();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_point_fab);
@@ -77,17 +106,17 @@ public class MainActivity extends BaseActivity
         transaction.replace(R.id.fragment_container, pointListFragment);
         transaction.commit();
         pointListFragment.setOnPointClickListener(item -> {
-            if (menu != null) {
-                MenuItem menuItem = menu.findItem(R.id.action_points_list);
+            if (mMenu != null) {
+                MenuItem menuItem = mMenu.findItem(R.id.action_points_list);
                 if (menuItem != null) {
                     menuItem.setIcon(R.drawable.ic_list_points);
                 }
             }
-            selectedNavigationId = R.id.nav_map;
+            mSelectedNavigationId = R.id.nav_map;
             setPointsMapFragment(item);
 
             NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-            navigationView.setCheckedItem(selectedNavigationId);
+            navigationView.setCheckedItem(mSelectedNavigationId);
         });
     }
 
@@ -104,7 +133,7 @@ public class MainActivity extends BaseActivity
     @Override
     public boolean onCreateOptionsMenu(Menu _menu) {
         getMenuInflater().inflate(R.menu.main, _menu);
-        menu = _menu;
+        mMenu = _menu;
         return true;
     }
 
@@ -114,20 +143,21 @@ public class MainActivity extends BaseActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_points_list) {
-            if (selectedNavigationId == R.id.nav_map) {
+            if (mSelectedNavigationId == R.id.nav_map) {
                 setPointsListFragment();
                 item.setIcon(R.drawable.ic_map_points);
-                selectedNavigationId = R.id.nav_place_list;
-            } else if (selectedNavigationId == R.id.nav_place_list) {
+                mSelectedNavigationId = R.id.nav_place_list;
+            } else if (mSelectedNavigationId == R.id.nav_place_list) {
                 setPointsMapFragment();
                 item.setIcon(R.drawable.ic_list_points);
-                selectedNavigationId = R.id.nav_map;
+                mSelectedNavigationId = R.id.nav_map;
             }
             NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-            navigationView.setCheckedItem(selectedNavigationId);
+            navigationView.setCheckedItem(mSelectedNavigationId);
 
             return true;
         }
+
 //        else if (id == R.id.app_bar_search) {
 //            return true;
 //        }
@@ -139,27 +169,39 @@ public class MainActivity extends BaseActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        selectedNavigationId = item.getItemId();
+        mSelectedNavigationId = item.getItemId();
 
-        MenuItem upMenuItem = menu.findItem(R.id.action_points_list);
-        if (selectedNavigationId == R.id.nav_map) {
+        MenuItem upMenuItem = mMenu.findItem(R.id.action_points_list);
+        if (mSelectedNavigationId == R.id.nav_map) {
             setPointsMapFragment();
             if (upMenuItem != null) {
                 upMenuItem.setIcon(R.drawable.ic_list_points);
             }
-        } else if (selectedNavigationId == R.id.nav_place_list) {
+        } else if (mSelectedNavigationId == R.id.nav_place_list) {
             setPointsListFragment();
             if (upMenuItem != null) {
                 upMenuItem.setIcon(R.drawable.ic_map_points);
             }
-        } else if (selectedNavigationId == R.id.nav_slideshow) {
+        } else if (mSelectedNavigationId == R.id.nav_slideshow) {
 
-        } else if (selectedNavigationId == R.id.nav_manage) {
+        } else if (mSelectedNavigationId == R.id.nav_manage) {
 
-        } else if (selectedNavigationId == R.id.nav_share) {
-
-        } else if (selectedNavigationId == R.id.nav_send) {
-
+        } else if (mSelectedNavigationId == R.id.nav_sign_out) {
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Intent in = new Intent();
+                                in.setClass(MainActivity.this, FirebaseAuthActivity.class);
+                                startActivityWithLeftAnimation(in);
+                                finish();
+                            } else {
+                                Snackbar.make(mRootView, R.string.sign_out_failed, Snackbar.LENGTH_LONG).show();
+                            }
+                        }
+                    });
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
